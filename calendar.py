2e178a8231325221e356bc4face5c09807f4ea83
nabila ahmed <sumayaad2020@gmail.com>
2024-09-18 19:04:56 +0600

    first commit

---
+++ calendar.py
@@ -0,0 +1,5 @@
+import calendar
+year =int(input("enter year: "))
+months=int(input("enter month: "))
+
+print(calendar.month(year,months))
---
+++ current date and time .py
@@ -0,0 +1,13 @@
+from datetime import date,time,datetime
+today = date.today()
+print(today)
+
+now = datetime.now()
+print(now)
+print()
+print(f"Current year:{today.year}")
+print(f"Current month:{today.month}")
+print(f"Current day:{today.day}")
+print()
+
+
